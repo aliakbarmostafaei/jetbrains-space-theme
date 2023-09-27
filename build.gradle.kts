52d53565b8a278e2c44a071dd3c9c214ebe72ad5
Ali Akbar Mostafāëi <aliakbarmostafaei@gmail.com>
2023-09-27 15:32:01 +0330

    Fix Python plugin dependency and syntax highlighting

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,12 @@ intellij {
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+
+    when (type.get()){
+        "PY" -> plugins.add("Pythonid")
+        "PC" -> plugins.add("PythonCore")
+        else -> plugins.add("PythonCore:${libs.versions.python.get()}")
+    }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
